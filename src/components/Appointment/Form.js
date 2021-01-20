c1bb0a45987071d53aa51370f89aaffe0a2a288b
khaoula <khaoula.mouanniss@gmail.com>
2021-01-20 00:18:28 -0500

    update the Form compenent to make it dynamic

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,24 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import InterviewerList from "../InterviewerList";
 import Button from "../Button";
 
 export default function Form(props){
 
+
+
+  const [name, setName] = useState(props.name || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
   return(
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()} >
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
             type="text"
             placeholder="Enter Student Name"
-            /*
-              This must be a controlled component
-            */
+            value={name}
+            onChange={(event) => setName(event.target.value)}
           />
         </form>
-        <InterviewerList interviewers={props.interviewers} interviewer={props.interviewer} setInterviewer={props.setInterviewer} />
+        <InterviewerList interviewers={props.interviewers} interviewer={interviewer} setInterviewer={setInterviewer} />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
