20364efbd5bc72c03be4fa424e16278dfbb92991
khaoula <khaoula.mouanniss@gmail.com>
2021-01-21 23:01:32 -0500

    use the back function in the application and add the selector getInterviewersByDay and test it

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,64 +1,36 @@
-/*const state = {
-  days: [
-    {
-      id: 1,
-      name: "Monday",
-      appointments: [1, 2, 3]
-    },
-    {
-      id: 2,
-      name: "Tuesday",
-      appointments: [4, 5]
-    }
-  ],
-  appointments: {
-    "1": { id: 1, time: "12pm", interview: null },
-    "2": { id: 2, time: "1pm", interview: null },
-    "3": {
-      id: 3,
-      time: "2pm",
-      interview: { student: "Archie Cohen", interviewer: 2 }
-    },
-    "4": { id: 4, time: "3pm", interview: null },
-    "5": {
-      id: 5,
-      time: "4pm",
-      interview: { student: "Chad Takahashi", interviewer: 2 }
-    }
-  },
-  interviewers: {
-    "1": {  
-      "id": 1,
-      "name": "Sylvia Palmer",
-      "avatar": "https://i.imgur.com/LpaY82x.png"
-    },
-    "2": {
-      id: 2,
-      name: "Tori Malcolm",
-      avatar: "https://i.imgur.com/Nmx0Qxo.png"
-    }
-  }
-};*/
 
-//appointments = dayfound.appointments.map(appointmentId => state.appointments[apointmentId]
- export function getAppointmentsForDay(state, day) {
 
-  if (state.days.length !== 0) {
-   
-    const filteredDay = state.days.filter(dayI => dayI.name === day);
+export function getAppointmentsForDay(state, day) {
 
-    if(filteredDay.length !== 0){
+  const filteredDay = state.days.find(dayState => day === dayState.name);
+  if (filteredDay) {
+    const appointments = filteredDay.appointments.map(appointmentId => state.appointments[appointmentId]);
 
-      const filteredApointments = Object.values(state.appointments).filter(appointment => filteredDay[0].appointments.indexOf(appointment.id) > -1);
-
-      return filteredApointments;
-    }
+    return appointments;
   }
   return [];
+  };
+
+
+ export function getInterviewersForDay(state, day) {
+
+  if(state.days.length > 0){
+    const appointments = getAppointmentsForDay(state,day);
+    const interviewersId = appointments.map(appointment => appointment.interview).filter(interviewer => interviewer !== null).map(interview => interview.interviewer);
+    const interviewersFiltered = Object.values(state.interviewers).filter(interviewer => interviewersId.indexOf(interviewer.id) > -1);
+   
+
+    return interviewersFiltered;
+
+  }
+  return [];
+    
+
+ 
+  
 };
 
-
- export function getInterview(state, interview) {
+  export function getInterview(state, interview) {
 
   if(interview){  
     const interviewer = Object.values(state.interviewers).filter(interviewer => interviewer.id === interview.interviewer);
@@ -66,5 +38,3 @@
   }    
   return null;
 };
-
-//interviewerlist = object.values(interviewers)
--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -5,11 +5,18 @@ export default function useVisualMode(initial) {
   
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
-  
-  const transition = (newMode) => {
-    setHistory(prev => [ ...prev,newMode]);
-    setMode(newMode);
-  }
+
+  const transition = (newMode, replace = false) => {
+    if (replace){
+      let newHistory = history.slice(0,- 1);
+      setHistory([...newHistory,newMode]);
+      setMode(newMode);
+    } else {
+      setHistory(prev => [ ...prev,newMode]);
+      setMode(newMode);
+    }
+    }
+    
   const back = () => {
     if(history.length > 1){
       let newHistory = history.slice(0,- 1);
